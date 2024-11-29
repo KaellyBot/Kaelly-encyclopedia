c87d790e9bac7b5750085f1094197a904bf250db
Steven Pescheteau <steven.pescheteau@gmail.com>
2024-11-29 20:12:23 +0100

    Sets images based on dofusdudeID and game

--- models/entities/sets.go
+++ models/entities/sets.go
@@ -1,6 +1,9 @@
 package entities
 
+import amqp "github.com/kaellybot/kaelly-amqp"
+
 type Set struct {
-	DofusDudeID int32 `gorm:"primaryKey"`
+	DofusDudeID int32     `gorm:"primaryKey"`
+	Game        amqp.Game `gorm:"primaryKey"`
 	Icon        string
 }
